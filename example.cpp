f613a110e460cd51b55a3250a320957c6205d5d2
zawpi <155113964+zawpi@users.noreply.github.com>
2025-06-06 20:21:40 +0200

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -2,14 +2,8 @@
 #include <iostream>
 
 int main() {
-	std::cout << "Name of the function: ";
-	std::string input;
-	std::cin >> input;
-
-	DWORD syscallId = GetSyscallId(input);
-	std::cout << "Syscall ID : "<< syscallId << std::endl;
-
-	std::cout << "Press Enter to exit...\n";
-	std::cin.ignore();
-	std::cin.get();
+    std::string funcName = "NtOpenProcess";
+    DWORD syscallID = GetSysCode(funcName);
+    std::cout << "Syscall ID: " << syscallID << std::endl;
+    return 0;
 }
